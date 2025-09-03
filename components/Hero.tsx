134852e68a523fb166dbb495b73481edd17aa9be
BerzanIrma <irmabia2005@gmail.com>
2025-09-03 15:55:05 +0300

    Navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,11 +19,22 @@ const handleScroll = () => {
             Discover the best deals on luxury cars, sports cars, and more.
           </p>
 
+
+
           <CustomButton
             title="Explore cars"
             containerStyles="bg-orange-500 text-white rounded-full mt-10"
             handleClick={handleScroll}
           />
+        </div>
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image src="/orange.svg" alt="orange background" fill className="object-contain absolute top-0 left-0 z-0 -translate-y-7 translate-x-16"
+  />
+            <Image src="/car.svg" alt="car" fill className="object-contain absolute top-0 left-0 z-0 -translate-y-5"/>
+            
+
+          </div>
         </div>
       </div>
   )
---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import Link from "next/link";
+import Image from "next/image";
+
+import CustomButton from "./CustomButton";
+
+const Navbar = () => {
+  return (
+    <header className='w-full absolute z-10'>
+      <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
+        
+        {/* Logo */}
+        <Link href='/' className='flex justify-center items-center gap-2'>
+          <Image
+            src='/logo2.png'
+            alt='Car hub Logo'
+            width={79}
+            height={18}
+            className='object-contain'
+          />
+           <span className='text-black text-lg font-bold'>Carhub</span>
+        </Link>
+
+        {/* Butoane */}
+        <div className="flex items-center gap-4">
+          <CustomButton text='Sign In' />
+          <button 
+            type='button' 
+            className="bg-orange-500 rounded-full bg-white min-w-[130px] py-2 px-4"
+          >
+            Sign Up
+          </button>
+        </div>
+
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar
+
